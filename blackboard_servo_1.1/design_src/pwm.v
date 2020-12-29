c9998918de46fc5097e1cb9dff54d66601d1ce46
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2020-12-28 22:12:41 -0800

    Update pwm.v

--- blackboard_servo_1.1/design_src/pwm.v
+++ blackboard_servo_1.1/design_src/pwm.v
@@ -1,26 +1,22 @@
 
 module pwm
-#(parameter
-    integer RES = 8
-)(
-    input wire clk, rst, en,
-
-    input wire [RES - 1 : 0] pulsewidth,
-    input wire [RES - 1 : 0] period,
-
-    output wire pwm_sig
+(
+    input  wire          clk, rst, en,
+    input  wire [11 : 0] period,
+    input  wire [11 : 0] pulsewidth,
+    output wire          pwm_sig
 );
     
-    reg [RES - 1 : 0] count;
+    reg [11 : 0] pulse_count = 0;
 
-    always @( posedge pwm_clk )
+    always @( posedge clk )
     begin
-        if (rst | (count == period))
-            count <= 0;
+        if (rst || (pulse_count == period))
+            pulse_count <= 0;
         else
-            count <= count + 1;
+            pulse_count <= pulse_count + 1;
     end
 
-    assign pwm_sig = (~rst & en) & (count < pulsewidth);
+    assign pwm_sig = ~rst & en & (pulse_count < pulsewidth);
 
-endmodule
+endmodule // pwm
