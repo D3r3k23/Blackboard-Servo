c6309de2a16168532c4a84a08e21851e985e41c1
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2020-12-28 22:13:23 -0800

    Update servo_controller.v

--- blackboard_servo_1.1/design_src/servo_controller.v
+++ blackboard_servo_1.1/design_src/servo_controller.v
@@ -1,25 +1,27 @@
 
 module servo_controller
 (
-    input  wire clk_100M, rst, en,
+    input  wire         clk_100M, rst, en,
     input  wire [7 : 0] angle, // 0 - 180
-    input  wire [6 : 0] speed, // 0 - 100
-    output wire out_sig
+    input  wire [3 : 0] speed, // 0 - 10
+    output wire         out_sig
 );
 
-    wire rotation_clk_div = (100 - speed) * 500000;
+    localparam integer MIN_PW = 70;
+    localparam integer MAX_PW = 230;
+
     wire rotation_clk;
 
     clk_divider rotation_clk_gen
     (
-        .clk_in  ( clk_100M         ),
-        .rst     ( rst              ),
-        .clk_div ( rotation_clk_div ),
-        .clk_out ( rotation_clk     )
+        .clk_in  ( clk_100M     ),
+        .rst     ( rst          ),
+        .clk_div ( 5000000      ), // 20 Hz
+        .clk_out ( rotation_clk )
     );
 
-    wire [5 : 0] target_pulsewidth = angle / 6;
-    reg  [4 : 0] pulsewidth;
+    wire [11 : 0] target_pulsewidth = (angle * 8) / 9 + MIN_PW; // 70 - 230 (angle: 0 - 180)
+    reg  [11 : 0] pulsewidth = 0;
 
     always @( posedge rotation_clk )
     begin
@@ -27,9 +29,20 @@ module servo_controller
             pulsewidth <= 0;
         else
         begin
-            if      (pulsewidth < pulsewidth_target) pulsewidth <= pulsewidth + 1;
-            else if (pulsewidth > pulsewidth_target) pulsewidth <= pulsewidth - 1;
-            else                                     pulsewidth <= pulsewidth;
+            if (speed == 0)
+                pulsewidth <= pulsewidth;
+
+            else if (pulsewidth < MIN_PW) pulsewidth <= MIN_PW;
+            else if (pulsewidth > MAX_PW) pulsewidth <= MAX_PW;
+        
+            else if (((target_pulsewidth - speed) <= pulsewidth) && (pulsewidth <= (target_pulsewidth + speed)))
+                pulsewidth <= target_pulsewidth;
+
+            else if (pulsewidth < target_pulsewidth) pulsewidth <= pulsewidth + speed;
+            else if (pulsewidth > target_pulsewidth) pulsewidth <= pulsewidth - speed;
+            
+            else
+                pulsewidth <= pulsewidth;
         end
     end
 
@@ -39,19 +52,18 @@ module servo_controller
     (
         .clk_in  ( clk_100M ),
         .rst     ( rst      ),
-        .clk_div ( 62500    ), // 1.6 kHz pwm frequency - 20 ms period, 5 bit resolution
+        .clk_div ( 1000     ), // 100 kHz pwm frequency
         .clk_out ( pwm_clk  )
     );
 
-    pwm #( .RES(5) )
-    sig_gen
+    pwm sig_gen
     (
-        .clk        ( pwm_clk    ),
+        .clk        ( pwm_clk    ), // 100 kHz | 10 us period
         .rst        ( rst        ),
         .en         ( en         ),
-        .pulsewidth ( pulsewidth ),
-        .period     ( 5'd32      ),
+        .period     ( 12'd2000   ), // PWM signal period: 20 ms
+        .pulsewidth ( pulsewidth ), // 70 - 230 | 700 - 2300 us
         .pwm_sig    ( out_sig    )
     );
 
-endmodule
+endmodule // servo_controller
