f260a314ae892f690a04cb06d354ff2ed376655f
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2020-12-28 23:54:42 -0800

    Update main.c

--- test_application/main.c
+++ test_application/main.c
@@ -1,1 +1,26 @@
 
+#include "servo.h"
+
+
+void delay(int x)
+    { for (int i = 0; i < 2000000 * x; i++); }
+
+
+int main(void)
+{
+    servo_init();
+    delay(10);
+  
+    servo_en(1);
+    servo_set_speed(1, 8);
+  
+    while(1)
+    {
+        servo_set_angle(0);
+        delay(5);
+        servo_set_angle(180);
+        delay(5);
+    }
+
+    return 0;
+}
